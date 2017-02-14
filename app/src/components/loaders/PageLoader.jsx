ff5eed67d7ad9f5038a7a40edcc2f7c77a456baa
Glendale Acosta <gglendale17@gmail.com>
2017-02-13 20:30:22 -0800

    Changed colors and added functional loading bar.

--- app/src/components/loaders/PageLoader.jsx
+++ app/src/components/loaders/PageLoader.jsx
@@ -3,11 +3,36 @@ import React from 'react';
 export default class PageLoader extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            loadBarStyle: {
+                width: '0',
+                height: '2px'
+            }
+        }
+    }
+    componentDidMount(){
+        var that = this;
+        setTimeout(()=>{
+
+            that.setState({
+                loadBarStyle:{
+                    width: '300px',
+                    height: '2px',
+                    transition: '1000ms ease-in-out'
+                }
+            })
+        }, 800)
     }
     render(){
+        var {loadBarStyle} = this.state;
+        
         return (
             <div className="page-loader">
-                <h1>Loading...</h1>
+                <h1 className="load-text">LOADING<span className="load-elipses">...</span></h1>
+                <div className="load-bar-container">
+
+                    <div className="load-bar" style={loadBarStyle}></div>
+                </div>
             </div>
         )
     }
